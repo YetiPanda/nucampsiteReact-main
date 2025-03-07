24e514dee6b59f8e6abb68bddabdf108e9bb2853
YetiPanda <jessegarza@protonmail.com>
2025-03-07 00:00:57 -0600

    campsitesReducer

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,17 +1,29 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from "@reduxjs/toolkit";
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+};
 
 export const selectAllCampsites = () => {
-    return CAMPSITES;
+  return CAMPSITES;
 };
+
+const campsitesSlice = createSlice({
+  name: "campsites",
+  initialState,
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 // export const selectRandomCampsite = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // };
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite)=> campsite.id === parseInt(id));
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured);
+  return CAMPSITES.find((campsite) => campsite.featured);
 };
