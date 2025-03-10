6abd14520ec723142c7ba55f133bf794c52d03bb
YetiPanda <jessegarza@protonmail.com>
2025-03-10 10:45:22 -0500

    refactor Header component for improved readability and structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,54 @@
-import { useState } from 'react';
-import {Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
-import { NavLink } from 'reactstrap';
-import NucampLogo from '../app/assets/img/logo.png';
-import UserLoginForm from '../features/user/UserLoginForm';
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import { NavLink } from "reactstrap";
+import NucampLogo from "../app/assets/img/logo.png";
+import UserLoginForm from "../features/user/userLoginForm";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <Navbar dark color ='primary' sticky='top' expand='md'>
-          <NavbarBrand className='ms-5' href='/'>
-            <img src={NucampLogo} alt='nucamp logo' className='float-start'/>
-            <h1 className='mt-1'>NuCamp</h1>
-          </NavbarBrand>
+  return (
+    <Navbar dark color="primary" sticky="top" expand="md">
+      <NavbarBrand className="ms-5" href="/">
+        <img src={NucampLogo} alt="nucamp logo" className="float-start" />
+        <h1 className="mt-1">NuCamp</h1>
+      </NavbarBrand>
 
-          <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-          <Collapse isOpen={menuOpen} navbar>
-
-            <Nav className='ms-auto' navbar>
-              <NavItem>
-                  <NavLink className='nav-link' href='/'>
-                      <i className='fa fa-home fa-lg' /> Home
-                  </NavLink>
-              </NavItem>
-              <NavItem>
-                  <NavLink className='nav-link' href='/directory'>
-                      <i className='fa fa-list fa-lg' /> Directory
-                  </NavLink>
-              </NavItem>
-              <NavItem>
-                  <NavLink className='nav-link' href='/about'>
-                      <i className='fa fa-info fa-lg' /> About
-                  </NavLink>
-              </NavItem>
-              <NavItem>
-                  <NavLink className='nav-link' href='/contact'>
-                      <i className='fa fa-address-card fa-lg' /> Contact
-                  </NavLink>
-              </NavItem>
-            </Nav>
-            <UserLoginForm />
-          </Collapse>
-      </Navbar>
-    )
-}
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" href="/">
+              <i className="fa fa-home fa-lg" /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" href="/directory">
+              <i className="fa fa-list fa-lg" /> Directory
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" href="/about">
+              <i className="fa fa-info fa-lg" /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" href="/contact">
+              <i className="fa fa-address-card fa-lg" /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <UserLoginForm />
+      </Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
